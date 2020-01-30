2ecd74868294c136ddc229c4e6767040f7bdb978
pmsfbmoll <pmoya@cifpfbmoll.eu>
2020-01-30 19:40:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/interactiverows/ViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.interactiverows;
+
+import android.view.View;
+import android.widget.RatingBar;
+
+class ViewHolder {
+    RatingBar rate=null;
+
+    ViewHolder(View base) {
+        this.rate = (RatingBar) base.findViewById(R.id.rate);
+    }
+}
